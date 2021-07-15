95c07a6dfeab30365566a151d0a3ff27c0dff775
Agnes Bergqvist <agnes.bergqvist@hotmail.com>
2021-07-15 13:29:02 +0200

    Added structure for template for products

---
+++ src/Components/Webshop/Template.js
@@ -0,0 +1,23 @@
+import React from "react"; 
+
+export default function ProductList () { 
+    return ( 
+
+        <article>
+            
+            <div>
+                <img></img>
+            </div>
+
+            <div>
+                <h1>Plant headline</h1>
+                <p>Description</p>
+                <p>Price: </p>
+            </div>
+
+        </article>
+
+        
+
+    )
+}
