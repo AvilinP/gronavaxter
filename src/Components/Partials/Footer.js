c3f2845eee6e49515422d65d4ebbc6052890b8d0
Avilin <avilin.persson@gmail.com>
2021-07-27 16:33:35 +0200

    Fixed font & responsive design

--- src/Components/Partials/Footer.js
+++ src/Components/Partials/Footer.js
@@ -5,12 +5,13 @@ function Footer() {
         <>
 
             <div className="card">
-                <div className="row g-0">
-                    <div className="col-7">
+               
+                <div className="row footer-row g-0">
+                    <div className="col">
                         <img src="/Images/footer.jpg" className="img-fluid" alt="Bild på Persian Shield blomma"/> 
                     </div>
 
-                    <div className="col-5 bg-footer lg-p-5">
+                    <div className="col bg-footer lg-p-5">
                         <div className="card-body">
                             <h2 className="card-title footer-title info_text_start">KONTAKTA OSS</h2>
 
@@ -19,13 +20,12 @@ function Footer() {
 
                             <h5 className="card-text footer-title2 info_text_start">ADRESS</h5>
                             <p className="card-text footer-text info_text_start">Gröna Gränd 1, 123 45 Grönskogen</p>
-
-
-
                         </div>
                     </div>
                 </div>
             </div>
+
+
 
         </>
     )
--- src/Components/Webshop/Homepage.js
+++ src/Components/Webshop/Homepage.js
@@ -22,31 +22,31 @@ function Homepage() {
                     <div className="col-sm-6">
                         <img src="/Images/vänsterbild_start.jpg" alt="bild" className="img-fluid" />
                     </div>
-                    <div className="col-sm-6 info_text_start"><h3>Vi erbjuder färg till trädgården och inne. Så du alltid ser cool ut.</h3></div>
+                    <div className="col-sm-6 pt-4 info_text_start"><h3>Vi erbjuder färg till trädgården och inne. Så du och dina växter alltid har det allra bästa.</h3></div>
                 </div>
 
 
-                <div className="row d-flex justify-content-center align-items-center">
+                <div className="row info-row-start d-flex justify-content-center align-items-center">
                     <div className="col-sm-6"><h3 className="pt-4 info_text_start">Våra växer kommer med näring och vägledning hur du ska ta hand om dom. Bra va?</h3></div>
                     <div className="col-sm-6">
-                        <img src="/Images/högerbild_start.jpg" alt="bild" className="img-fluid"/>
+                        <img src="/Images/högerbild_start.jpg" alt="bild" className="img-fluid pt-4"/>
                     </div>
                 </div>
             </div>
 
-            <div className="d-flex flex-column justify-content-center align-items-center p-5">
-                <h1 className="pb-3 info_text_start">TRÄDGÅRDS- & LANDSKAPSDESIGN</h1>
+            <div className="d-flex flex-column justify-content-center align-items-center p-3">
+                <h1 className="pb-3 pt-3 info_text_start text-center">TRÄDGÅRDS- & LANDSKAPSDESIGN</h1>
 
 
                 <div className="card mb-3" style={{ maxWidth: "auto" }}>
-                    <div className="row g-0">
+                    <div className="row g-0 m-0">
                         <div className="col-md-7">
                             <img src="/Images/landskapsdesign_start.jpg" className="img-fluid" alt="..." />
                         </div>
                         <div className="col-md-5 bg-landscaping">
                             <div className="card-body ">
                                 <h5 className="card-title info_text_start">Planera trädgård - boka hembesök</h5>
-                                <p className="card-text info_text_start">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p className="card-text info_text_start"> Skapat otaliga Pinterest-boards men vet inte var du ska börja för att skapa din drömträdgård? Misströsta inte, vi finns här för att hjälpa dig!</p>
                             </div>
                         </div>
                     </div>
@@ -54,13 +54,13 @@ function Homepage() {
             </div>
 
             <div className="d-flex flex-column justify-content-center align-items-center p-5">
-                <h1 className="pb-3 info_text_start">VAD KAN VI HJÄLPA DIG MED?</h1>
-                <p className="info_text_start">We offer highly recommended products that are specially crafted to nourish and moisturize your skin.</p>
+                <h1 className="pb-3 info_text_start text-center">VAD KAN VI HJÄLPA DIG MED?</h1>
+                <p className="info_text_start text-center text-justify"> Är du på jakt efter livets finaste kruka eller försöker du rädda en döende växt? <br/> Vilket som finns vi alltid på plats för dina frågor om blommor och växter! </p>
 
                 <div className="d-flex flex-lg-row flex-column">
 
                     <div className="card m-2" style={{ width: "18rem" }}>
-                        <img src="/Images/landskapsdesign_start.jpg" className="card-img-top" alt="Blommor ute" />
+                        <img src="/Images/blommor_ute.jpeg" className="card-img-top" alt="Blommor ute" />
                         <div className="card-body d-flex flex-column justify-content align-items-center">
                             <h5 className="card-title text-center info_text_start">Blommor ute</h5>
                             <p className="card-text text-center info_text_start">Sätt färg på trädgården med vackra blommor.</p>
@@ -68,7 +68,7 @@ function Homepage() {
                     </div>
 
                     <div className="card m-2" style={{ width: "18rem" }}>
-                        <img src="/Images/landskapsdesign_start.jpg" className="card-img-top" alt="Gödsel och näring" />
+                        <img src="/Images/blomjord.jpeg" className="card-img-top" alt="Gödsel och näring" />
                         <div className="card-body d-flex flex-column justify-content align-items-center">
                             <h5 className="card-title text-center info_text_start">Gödsel och näring</h5>
                             <p className="card-text text-center info_text_start">Ge dina växter gödsel och näring i år.</p>
@@ -76,7 +76,7 @@ function Homepage() {
                     </div>
 
                     <div className="card m-2" style={{ width: "18rem" }}>
-                        <img src="/Images/landskapsdesign_start.jpg" className="card-img-top" alt="Krukor" />
+                        <img src="/Images/blomkruka.jpeg" className="card-img-top" alt="Krukor" />
                         <div className="card-body d-flex flex-column justify-content align-items-center">
                             <h5 className="card-title text-center info_text_start">Krukor</h5>
                             <p className="card-text text-center info_text_start">Botanisera bland säsongens nya krukor!</p>
